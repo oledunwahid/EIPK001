d1d0cda50556d6f4c7f4f1bda5efbc6858868954
oledunwahid <makkawirelang@gmai.com>
2024-01-12 10:25:19 +0700

    update support

--- tiket/akses_menu_support.php
+++ tiket/akses_menu_support.php
@@ -1,83 +1,149 @@
 <?php
 $page = isset($_GET['page']) ? $_GET['page'] : '';
 
-if (isset($_SESSION['username'])) {
-	if ($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'user') {
-		switch ($page) {
-			case 'Dashboard':
-				include "home-facility.php";
-				break;
-			case 'ITSupport':
-				include "it.php";
-				break;
-			case 'AccessAdministrator':
-				include 'akses_admin.php';
-				break;
-			case 'MenuAdministrator':
-				include 'menu_admin.php';
-				break;
-			case 'AddSupport':
-				include 'add_support.php';
-				break;
-			case 'EditTicketIT':
-				include 'edit_detail_tiket.php';
-				break;
-			case 'ViewTicketIT':
-				include 'detail_tiket.php';
-				break;
-			case 'ATK/Stationary':
-				include 'stationary_facilities.php';
-				break;
-			case 'EditATK/Stationary':
-				include 'edit_stationary_facilities.php';
-				break;
-			case 'ViewATK/Stationary':
-				include 'view_stationary_facilities.php';
-				break;
-			case 'ATKList':
-				include 'atk.php';
-				break;
-			case 'Building Facilities':
-				include 'building_facilities.php';
-				break;
-			case 'Edit Building Facilities':
-				include 'edit_building_facilities.php';
-				break;
-			case 'View Building Facilities':
-				include 'view_building_facilities.php';
-				break;
-			case 'Other Facilities':
-				include 'other_facilities.php';
-				break;
-			case 'Edit Other Facilities':
-				include 'edit_other_facilities.php';
-				break;
-			case 'View Other Facilities':
-				include 'view_other_facilities.php';
-				break;
-			case 'Delivery':
-				include 'kurir.php';
-				break;
-			case 'Received':
-				include 'received_kurir.php';
-				break;
-			case 'EditReceived':
-				include 'edit_received_kurir.php';
-				break;
-			case 'ViewReceived':
-				include 'view_received_kurir.php';
-				break;
-			case 'EditKurir':
-				include 'edit_detail_kurir.php';
-				break;
-			case 'ViewKurir':
-				include 'detail_kurir.php';
-				break;
-			default:
-				include "pages-404.php";
-				break;
-		}
+$sql7 = mysqli_query($koneksi, "SELECT * FROM access_level WHERE idnik = $niklogin");
+$row7 = mysqli_fetch_assoc($sql7);
+
+
+if (isset($row7['admin']) && ($row7['admin'] == '1')) {
+	switch ($page) {
+		case 'Dashboard':
+			include "home-facility.php";
+			break;
+		case 'ITSupport':
+			include "it.php";
+			break;
+		case 'AccessAdministrator':
+			include 'akses_admin.php';
+			break;
+		case 'MenuAdministrator':
+			include 'menu_admin.php';
+			break;
+		case 'AddSupport':
+			include 'add_support.php';
+			break;
+		case 'EditTicketIT':
+			include 'edit_detail_tiket.php';
+			break;
+		case 'ViewTicketIT':
+			include 'detail_tiket.php';
+			break;
+		case 'ATK/Stationary':
+			include 'stationary_facilities.php';
+			break;
+		case 'EditATK/Stationary':
+			include 'edit_stationary_facilities.php';
+			break;
+		case 'ViewATK/Stationary':
+			include 'view_stationary_facilities.php';
+			break;
+		case 'ATKList':
+			include 'atk.php';
+			break;
+		case 'Building Facilities':
+			include 'building_facilities.php';
+			break;
+		case 'Edit Building Facilities':
+			include 'edit_building_facilities.php';
+			break;
+		case 'View Building Facilities':
+			include 'view_building_facilities.php';
+			break;
+		case 'Other Facilities':
+			include 'other_facilities.php';
+			break;
+		case 'Edit Other Facilities':
+			include 'edit_other_facilities.php';
+			break;
+		case 'View Other Facilities':
+			include 'view_other_facilities.php';
+			break;
+		case 'Delivery':
+			include 'kurir.php';
+			break;
+		case 'Received':
+			include 'received_kurir.php';
+			break;
+		case 'EditReceived':
+			include 'edit_received_kurir.php';
+			break;
+		case 'ViewReceived':
+			include 'view_received_kurir.php';
+			break;
+		case 'EditKurir':
+			include 'edit_detail_kurir.php';
+			break;
+		case 'ViewKurir':
+			include 'detail_kurir.php';
+			break;
+		default:
+			include "pages-404.php";
+			break;
 	}
 } else {
-	include "login.php";
+	switch ($page) {
+		case 'Dashboard':
+			include "home-facility.php";
+			break;
+		case 'ITSupport':
+			include "it.php";
+			break;
+		case 'AddSupport':
+			include 'add_support.php';
+			break;
+		case 'EditTicketIT':
+			include 'edit_detail_tiket.php';
+			break;
+		case 'ViewTicketIT':
+			include 'detail_tiket.php';
+			break;
+		case 'ATK/Stationary':
+			include 'stationary_facilities.php';
+			break;
+		case 'EditATK/Stationary':
+			include 'edit_stationary_facilities.php';
+			break;
+		case 'ViewATK/Stationary':
+			include 'view_stationary_facilities.php';
+			break;
+		case 'Building Facilities':
+			include 'building_facilities.php';
+			break;
+		case 'Edit Building Facilities':
+			include 'edit_building_facilities.php';
+			break;
+		case 'View Building Facilities':
+			include 'view_building_facilities.php';
+			break;
+		case 'Other Facilities':
+			include 'other_facilities.php';
+			break;
+		case 'Edit Other Facilities':
+			include 'edit_other_facilities.php';
+			break;
+		case 'View Other Facilities':
+			include 'view_other_facilities.php';
+			break;
+		case 'Delivery':
+			include 'kurir.php';
+			break;
+		case 'Received':
+			include 'received_kurir.php';
+			break;
+		case 'EditReceived':
+			include 'edit_received_kurir.php';
+			break;
+		case 'ViewReceived':
+			include 'view_received_kurir.php';
+			break;
+		case 'EditKurir':
+			include 'edit_detail_kurir.php';
+			break;
+		case 'ViewKurir':
+			include 'detail_kurir.php';
+			break;
+		default:
+			include "pages-404.php";
+			break;
+	}
 }
--- tiket/layout/menu-support.php
+++ tiket/layout/menu-support.php
@@ -29,82 +29,87 @@
         <div class="container-fluid">
             <div id="two-column-menu">
             </div>
-            <?php if ($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'user') { ?>
-                <ul class="navbar-nav" id="navbar-nav">
-                    <li class="menu-title"><span data-key="t-menu">Menu</span></li>
-                    <li class="nav-item">
-                        <a class="nav-link menu-link<?php if ($page == 'Dashboard') echo 'active'; ?>" href="index.php?page=Dashboard" aria-expanded="false">
-                            <i class="ri-dashboard-2-line"></i> <span> Dashboard </span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link menu-link <?php if ($page == 'ITSupport') echo 'active'; ?>" href="index.php?page=ITSupport" aria-expanded="false">
-                            <i class=" ri-mac-line"></i> <span> IT-Support </span>
-                        </a>
-                    </li>
 
+            <ul class="navbar-nav" id="navbar-nav">
+                <li class="menu-title"><span data-key="t-menu">Menu</span></li>
+                <li class="nav-item">
+                    <a class="nav-link menu-link<?php if ($page == 'Dashboard') echo 'active'; ?>" href="index.php?page=Dashboard" aria-expanded="false">
+                        <i class="ri-dashboard-2-line"></i> <span> Dashboard </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link menu-link <?php if ($page == 'ITSupport') echo 'active'; ?>" href="index.php?page=ITSupport" aria-expanded="false">
+                        <i class=" ri-mac-line"></i> <span> IT-Support </span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link menu-link <?php if ($page == 'ATK/Stationary' || $page == 'Building Facilities' || $page == 'Other Facilities') echo 'active'; ?>" href="#ga2" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="ga2">
+                        <i class="ri-tools-fill"></i> <span> GA Facilities </span>
+                    </a>
+                    <div class="collapse <?php if ($page == 'Other Facilities' || $page == 'ATK/Stationary' || $page == 'Building Facilities') echo 'show'; ?> menu-dropdown" id="ga2">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="index.php?page=ATK/Stationary" class="nav-link <?php if ($page == 'ATK/Stationary') echo 'active'; ?>">ATK/Stationary</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="index.php?page=Building Facilities" class="nav-link <?php if ($page == 'Building Facilities') echo 'active'; ?>">Building Maintenance Support</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="index.php?page=Other Facilities" class="nav-link <?php if ($page == 'Other Facilities') echo 'active'; ?>">Other Facilities Request</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link menu-link <?php if ($page == 'Delivery' || $page == 'Received') echo 'active'; ?>" href="#d" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="d">
+                        <i class="mdi mdi-truck-fast-outline"></i>
+                        <span> Courier Service </span>
+                    </a>
+                    <div class="collapse <?php if ($page == 'Received' || $page == 'Delivery') echo 'show'; ?> menu-dropdown" id="d">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="index.php?page=Delivery" class="nav-link <?php if ($page == 'Delivery') echo 'active'; ?>">Sent Package</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="index.php?page=Received" class="nav-link <?php if ($page == 'Received') echo 'active'; ?>">Received Package</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <?php
+                $sql7 = mysqli_query($koneksi, "SELECT * FROM access_level WHERE idnik = $niklogin");
+                $row7 = mysqli_fetch_assoc($sql7);
+                ?>
+
+                <?php if (isset($row7['admin']) && ($row7['admin'] == '1')) : ?>
+                    <li class="menu-title"><span data-key="t-menu">Admin Access</span></li>
                     <li class="nav-item">
-                        <a class="nav-link menu-link <?php if ($page == 'ATK/Stationary' || $page == 'Building Facilities' || $page == 'Other Facilities') echo 'active'; ?>" href="#ga2" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="ga2">
-                            <i class="ri-tools-fill"></i> <span> GA Facilities </span>
+                        <a class="nav-link menu-link<?= ($page == 'Administrator') ? ' active' : ''; ?>" href="#admin" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="admin">
+                            <i class="ri-admin-fill"></i> <span>Administrator</span>
                         </a>
-                        <div class="collapse <?php if ($page == 'Other Facilities' || $page == 'ATK/Stationary' || $page == 'Building Facilities') echo 'show'; ?> menu-dropdown" id="ga2">
+                        <div class="collapse menu-dropdown" id="admin">
                             <ul class="nav nav-sm flex-column">
                                 <li class="nav-item">
-                                    <a href="index.php?page=ATK/Stationary" class="nav-link <?php if ($page == 'ATK/Stationary') echo 'active'; ?>">ATK/Stationary</a>
+                                    <a href="index.php?page=AccessAdministrator" class="nav-link"> Access Admin </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="index.php?page=Building Facilities" class="nav-link <?php if ($page == 'Building Facilities') echo 'active'; ?>">Building Maintenance Support</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="index.php?page=Other Facilities" class="nav-link <?php if ($page == 'Other Facilities') echo 'active'; ?>">Other Facilities Request</a>
+                                    <a href="index.php?page=MenuAdministrator" class="nav-link"> Menu Admin </a>
                                 </li>
                             </ul>
                         </div>
                     </li>
-
                     <li class="nav-item">
-                        <a class="nav-link menu-link <?php if ($page == 'Delivery' || $page == 'Received') echo 'active'; ?>" href="#d" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="d">
-                            <i class="mdi mdi-truck-fast-outline"></i>
-                            <span> Courier Service </span>
+                        <a class="nav-link menu-link <?php if ($page == 'ATKList') echo 'active'; ?>" href="index.php?page=ATKList" aria-expanded="false">
+                            <i class="ri-scissors-fill"></i> <span> ATK List </span>
                         </a>
-                        <div class="collapse <?php if ($page == 'Received' || $page == 'Delivery') echo 'show'; ?> menu-dropdown" id="d">
-                            <ul class="nav nav-sm flex-column">
-                                <li class="nav-item">
-                                    <a href="index.php?page=Delivery" class="nav-link <?php if ($page == 'Delivery') echo 'active'; ?>">Sent Package</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="index.php?page=Received" class="nav-link <?php if ($page == 'Received') echo 'active'; ?>">Received Package</a>
-                                </li>
-                            </ul>
-                        </div>
                     </li>
+                <?php endif; ?>
 
-                    <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') : ?>
-                        <li class="menu-title"><span data-key="t-menu">Admin Access</span></li>
-                        <li class="nav-item">
-                            <a class="nav-link menu-link<?= ($page == 'Administrator') ? ' active' : ''; ?>" href="#admin" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="admin">
-                                <i class="ri-admin-fill"></i> <span>Administrator</span>
-                            </a>
-                            <div class="collapse menu-dropdown" id="admin">
-                                <ul class="nav nav-sm flex-column">
-                                    <li class="nav-item">
-                                        <a href="index.php?page=AccessAdministrator" class="nav-link"> Access Admin </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="index.php?page=MenuAdministrator" class="nav-link"> Menu Admin </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link menu-link <?php if ($page == 'ATKList') echo 'active'; ?>" href="index.php?page=ATKList" aria-expanded="false">
-                                <i class="ri-scissors-fill"></i> <span> ATK List </span>
-                            </a>
-                        </li>
-                    <?php endif; ?>
+            </ul>
 
-                </ul>
-            <?php    } ?>
 
         </div>
         <!-- Sidebar -->
